05a80a72fa8aba38fd2e7fb71bd0595a76af4a71
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-09-05 15:02:02 -0700

    cancer.py clarified "indicated for treatment of cancer"

--- transplant_llm/medication/ingredients/cancer.py
+++ transplant_llm/medication/ingredients/cancer.py
@@ -11,5 +11,5 @@ class RxClassCancer(StrEnum):
     CHECKPOINT = 'Checkpoint inhibitors, especially PD-1, PDL-1, CTLA-4'
     CYTOKINE = 'Cytokine therapy, especially IL-2 and interferon alpha'
     CAR_T = 'Chimeric antigen receptor (CAR-T)'
-    OTHER = 'Other cancer drug'
+    OTHER = 'Other drug indicated for treatment of cancer(s)'
     NONE = 'None of the above'
