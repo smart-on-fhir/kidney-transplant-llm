5fed942f94ad9103f82fec7798f7e5e1febb629e
Dylan Phelan <Dtphelan1@gmail.com>
2025-11-05 15:00:55 -0500

    default_factory used instead of immutable default value

--- kidney_transplant_llm/pydantic_study_variables.py
+++ kidney_transplant_llm/pydantic_study_variables.py
@@ -7,7 +7,7 @@ class SpanAugmentedMention(BaseModel):
         description="Whether there is any mention of this variable in the text."
     )   
     spans: list[str] = Field(
-        [],
+        default_factory=list
         description="The text spans where this variable is mentioned."
     )
 
