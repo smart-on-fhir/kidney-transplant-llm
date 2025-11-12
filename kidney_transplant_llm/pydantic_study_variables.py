07890eca0f24134f65d18446a32aa135b21e9285
Dylan Phelan <Dtphelan1@gmail.com>
2025-11-12 16:22:40 -0500

    Added language specifying that all donor variables should be scoped to the first renal transplant

--- kidney_transplant_llm/pydantic_study_variables.py
+++ kidney_transplant_llm/pydantic_study_variables.py
@@ -19,9 +19,13 @@ class SpanAugmentedMention(BaseModel):
 
 # Dates are treated as strings - no enum needed
 class DonorTransplantDateMention(SpanAugmentedMention):
+    """
+    Date of the first renal transplant. If there is only one transplant mentioned, 
+    assume that this is the first transplant. 
+    """
     donor_transplant_date: str | None = Field(
         None,
-        description='Exact date of renal transplant; use YYYY-MM-DD format in your response. Only highlight date mentions with an explicit day, month, and year (e.g. 2020-01-15). All other date mentions, or an absence of a date mention, should be indicated with None.'
+        description='Exact date of first renal transplant; use YYYY-MM-DD format in your response. Only highlight date mentions with an explicit day, month, and year (e.g. 2020-01-15). All other date mentions, or an absence of a date mention, should be indicated with None.'
     )
 
 class DonorType(StrEnum):
@@ -30,9 +34,13 @@ class DonorType(StrEnum):
     NOT_MENTIONED = "Donor was not mentioned as living or deceased"
 
 class DonorTypeMention(SpanAugmentedMention):
+    """
+    Type of donor in the first renal transplant. If there is only one transplant 
+    mentioned, assume that this is the first transplant. 
+    """
     donor_type: DonorType = Field(
         DonorType.NOT_MENTIONED,
-        description='Was the renal donor living at the time of transplant?'
+        description='Was the first renal donor living at the time of renal transplant?'
     )
 
 class DonorRelationship(StrEnum):
@@ -41,9 +49,13 @@ class DonorRelationship(StrEnum):
     NOT_MENTIONED = "Donor relationship status was not mentioned"
 
 class DonorRelationshipMention(SpanAugmentedMention):
+    """
+    Relatedness of the donor and recipient in the first renal transplant. If there is only one
+    transplant mentioned, assume that this is the first transplant. 
+    """
     donor_relationship: DonorRelationship = Field(
         DonorRelationship.NOT_MENTIONED,
-        description='Was the renal donor biologically related to the recipient?'
+        description='Was the first renal transplant donor biologically related to the recipient?'
     )
 
 class DonorHlaMatchQuality(StrEnum):
@@ -53,9 +65,13 @@ class DonorHlaMatchQuality(StrEnum):
     NOT_MENTIONED = "HLA match quality not mentioned"
 
 class DonorHlaMatchQualityMention(SpanAugmentedMention):
+    """
+    Human leukocyte antigen (HLA) match quality of the first renal transplant. If there is only one
+    transplant mentioned, assume that this is the first transplant. 
+    """
     donor_hla_match_quality: DonorHlaMatchQuality = Field(
         DonorHlaMatchQuality.NOT_MENTIONED,
-        description='What was the renal transplant HLA match quality?'
+        description='What was the HLA match quality for the first renal transplant?'
     )
 
 class DonorHlaMismatchCount(StrEnum):
@@ -69,9 +85,13 @@ class DonorHlaMismatchCount(StrEnum):
     NOT_MENTIONED = 'HLA mismatch count not mentioned'
 
 class DonorHlaMismatchCountMention(SpanAugmentedMention):
+    """
+    Human leukocyte antigen (HLA) mismatch count for the first renal transplant. If there is only 
+    one transplant mentioned, assume that this is the first transplant. 
+    """
     donor_hla_mismatch_count: DonorHlaMismatchCount = Field(
         DonorHlaMismatchCount.NOT_MENTIONED,
-        description='What was the renal donor-recipient HLA mismatch count?'
+        description='What was the donor-recipient HLA mismatch count for the first renal transplant?'
     )
 
 ###############################################################################
