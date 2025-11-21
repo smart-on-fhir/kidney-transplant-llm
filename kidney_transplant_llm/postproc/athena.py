27f43810e69cf04df330d55252ef6b14b5423cbf
comorbidity <andrew.mcmurry@tch.harvard.edu>
2025-11-20 23:28:20 -0800

    now supports choice pipeline of donor or longitudinal

--- kidney_transplant_llm/postproc/athena.py
+++ kidney_transplant_llm/postproc/athena.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+from kidney_transplant_llm.postproc import filetool
 from kidney_transplant_llm.postproc.schema import (
     SUBJECT_REF,
     DOCUMENT_REF,
@@ -7,18 +9,18 @@ from kidney_transplant_llm.postproc.schema import (
     DOC_ORDINAL,
     SAMPLE_COLS,
     HIGHLIGHT_COLS,
-    GPT_OSS_120B)
+    NLP_DONOR_GPT_OSS_120B)
 
-def select_from_athena(sample='irae__sample_casedef_index',
-                       highlights='irae__highlights_donor',
-                       origin=GPT_OSS_120B) -> str:
+def select_sql(highlights='irae__highlights_donor',
+               sample='irae__sample_casedef_index',
+               origin=NLP_DONOR_GPT_OSS_120B) -> str:
     """
     :param sample: SQL Table name of sample CaseDef
     :param highlights: SQL Table name of highlights LLM
     :param origin: default GPT_OSS_120B
     :return: str SELECT
     """
-    view = highlights + '_' + sample.replace('irae__sample_casedef_', '')
+    view = filetool.name_view(highlights, sample)
 
     sample_cols = [f'sample.{col}' for col in SAMPLE_COLS]
     sample_cols = '\n,'.join(sample_cols)
@@ -31,9 +33,21 @@ def select_from_athena(sample='irae__sample_casedef_index',
         "SELECT distinct",
         sample_cols, ',',
         highlight_cols,
-        f"FROM  {sample} as sample, {highlights} as highlights",
+        f"FROM  {highlights} as highlights, {sample} as sample",
         f"WHERE highlights.{NOTE_REF} = sample.{DOCUMENT_REF}",
         f"AND   origin='{origin}'",
         f"ORDER BY {SUBJECT_REF}, {SORT_BY_DATE}, {ENC_ORDINAL}, {DOC_ORDINAL}",
+        ";\n"
     ]
     return '\n'.join(_sql)
+
+def select_sql_file(highlights='irae__highlights_donor',
+                    sample='irae__sample_casedef_index',
+                    origin=NLP_DONOR_GPT_OSS_120B) -> Path:
+
+    view = filetool.name_view(highlights, sample)
+    text_sql = select_sql(highlights, sample, origin)
+    file_sql = filetool.path_highlights(f'{view}.sql')
+    with open(str(file_sql), 'w') as f:
+        f.write(text_sql)
+    return file_sql
--- kidney_transplant_llm/postproc/example.py
+++ kidney_transplant_llm/postproc/example.py
@@ -1,40 +1,50 @@
+from kidney_transplant_llm.postproc.schema import (
+    SAMPLE_PRE,
+    SAMPLE_INDEX,
+    SAMPLE_POST,
+    SUBJECT_REF,
+    ENCOUNTER_REF,
+    NLP_GPT_OSS_120B,
+    NLP_DONOR_GPT_OSS_120B)
 from kidney_transplant_llm.postproc import (
-    schema,
     athena,
     filetool,
     pivot_table,
     rank_llm)
 
-def highlights_donor_index():
-    task = 'irae__highlights_donor_index'
+def pipeline(highlights:str, sample:str, origin:str):
+    view = filetool.name_view(highlights, sample)
     print('######################################################################')
-    print('TASK: ', task)
-    print(f'Step1: create view {task}')
-    print(f'Input: {task}.sql')
-    print(f'Output: {task}.csv')
-    sql = athena.select_from_athena(
-        sample = 'irae__sample_casedef_index',
-        highlights = 'irae__highlights_donor')
+    print('VIEW: ', view)
+    print(f'Step1: create view {view}')
+    output_sql = athena.select_sql_file(highlights= highlights, sample=sample, origin=origin)
 
-    file_sql = filetool.path_highlights(f'{task}.sql')
-    with open(str(file_sql), 'w') as f:
-        f.write(sql)
-    print(file_sql)
+    print(f'Output: {output_sql}')
     print('######################################################################')
     print('Step2: Pivot CSV sublabel_name --> as columns')
-    print(f'Input: {task}.csv')
-    print(f'Output: {task}.pivot.csv')
-    output_csv = pivot_table.pivot_highlights_csv(highlights_csv=f'{task}.csv')
+    print(f'Input: {view}.csv')
+    output_csv = pivot_table.pivot_highlights_csv(highlights_csv=f'{view}.csv')
     print(output_csv)
 
     print('######################################################################')
     print('Step3: Rank LLM term frequency')
-    input_csv = filetool.path_highlights(f'{task}.pivot.csv')
-    output_csv = filetool.path_highlights(f'{task}.pivot.tf.csv')
-    output_df = rank_llm.count_tf(input_csv, stratifier=schema.SUBJECT_REF)
+    input_csv = filetool.path_highlights(f'{view}.pivot.csv')
+    output_csv = filetool.path_highlights(f'{view}.pivot.tf.csv')
+    output_df = rank_llm.count_tf(input_csv, stratifier=SUBJECT_REF)
     output_df.to_csv(output_csv, index=False)
     print(output_csv)
     print('######################################################################')
 
+def highlights_donor_index(highlights:str = 'irae__highlights_donor',
+                           sample:str = SAMPLE_INDEX,
+                           origin:str=NLP_DONOR_GPT_OSS_120B):
+    pipeline(highlights, sample, origin)
+
+def highlights_longitudinal(highlights:str = 'irae__highlights_longitudinal',
+                            sample:str = SAMPLE_POST,
+                            origin:str= NLP_GPT_OSS_120B):
+    pipeline(highlights, sample, origin)
+
 if __name__ == '__main__':
     highlights_donor_index()
+    highlights_longitudinal()
